2513c4ccb023c97e541486969c2c3c0b6f992c93
sainagaraju <nagarajusai91@gmail.com>
2026-05-31 22:10:49 +0530

    data types script

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -8,6 +8,8 @@ SUM=$(($NUM1+NUM2))
 echo "The Sume is: $SUM"
 
 NAMES=("sai" "mani" "Prabhkar" "Gopi" "Prasanna")
+NUMBERS=(1 2 3 4 5)
 
 echo "Names are: ${NAMES[@]}"
 echo "First Name: ${NAMES[0]}"
+echo "Numbers : ${NUMBERS[@]}"
