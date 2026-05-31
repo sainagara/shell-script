bf5a5ba00c6ec508fd1437cc74905d13ca2d7dcc
sainagaraju <nagarajusai91@gmail.com>
2026-05-31 20:38:05 +0530

    Add variable declaration script with conversation example

---
+++ 02-conversation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PERSON1="Sai"
+PERSON2="Mani"
+
+
+G=/e[32m
+Y=/e[33m
+N=/e{0m}
+
+
+echo -e ":$G $PERSON1 Hey $Y $PERSON2,$N what are you doing tonight?"
+echo -e ":$Y $PERSON2 Hey $G $PERSON1, $N nothing planned. what's up?"
+echo  -e"$G $PERSON1: Hey $Y $PERSON2, $N let's go watch a football match!"
+echo -e "$Y $PERSON2: Hey $G $PERSON1, $N that sounds great! which teams are playing?"
+echo -e "$G $PERSON1: Hey $Y $PERSON2, $N it's Manchester United vs Arsenal!"
+echo -e "$Y $PERSON2: Hey $G $PERSON1, $N perfect! I will bring the snacks!"
