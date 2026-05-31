ef51d73559bc16e68e40b2d5a5c5d6112d7e02b4
sainagaraju <nagarajusai91@gmail.com>
2026-05-31 21:19:35 +0530

    Reading The Values From the User

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Enter the User Name: "
+read USER_NAME
+
+echo "User Name is: $USERNAME"
+
+echo "Enter The Password: "
+read -s PASSWORD
+
+echo "The Password is: $PASSWORD"
