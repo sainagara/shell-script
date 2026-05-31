b9a2651670a56f6a971e93a3caa4901331d0eb5f
sainagaraju <nagarajusai91@gmail.com>
2026-05-31 22:16:34 +0530

    validating number is even or odd

---
+++ 08-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter the number: "
+read NUM
+
+#validating number is even or odd
+
+RES=NUM%2
+
+if[$RES eq 0]; then
+  echo "$NUM is Even Number"
+else
+ echo "$Num is Odd Number"
+fi
