3be11a81efd7ff48d64c7ef8c7ab4530c2406c09
sainagaraju <nagarajusai91@gmail.com>
2026-06-02 15:15:40 +0530

    Update On Installing Scrip Adding extra Installing Nginx script

---
+++ 09-install-functions.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+# Check root access or not
+if [ $USER_ID -ne 0 ]; then
+   echo "Please run this script with root access"
+
+fi
+
+# first arg -> what are you trying to install
+# second arg -> exit code
+
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+       echo "Installing $1 is Failure"
+
+    else
+       echo "Installing $1 is Succus"
+
+    fi
+}
+
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]; then
+   echo "MySql Already Installed...SKIPPING"
+
+else
+  echo "Installing Mysql"
+  dnf install mysql -y
+  VALIDATE mysql $?
+
+fi
+
+
+dnf list installed nginx
+
+if [ $? -eq 0 ]; then
+   echo "ngin is alredy Installed... SKIPPING"
+
+else
+   echo "Installing nginx"
+   dnf installe nginx -y
+   VALIDATE nginx $?
+
+fi
