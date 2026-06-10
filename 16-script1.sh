2958df0c64f6a53357a5f032125f5df3ad8a17b5
sainagaraju <nagarajusai91@gmail.com>
2026-06-11 03:11:39 +0530

    while loop script

--- 16-script1.sh
+++ 16-script1.sh
@@ -5,5 +5,8 @@ DIR_PATH="/etc/paaswd"
 echo "Printing from script 1 directory path: $DIR_PATH"
 echo "Script-1 PID is: $$"
 
-# sh 17-script2.sh
+#When we cal using sh, pid should be different and can't able to access variables
+# sh 17-script2.sh 
+
+#When we cal using source, pid should be same and can able to access variables
 source ./17-script2.sh
---
+++ 18-while-loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while IFS= read -r line
+ do 
+   
+   echo "$line"
+
+ done < 06-special-var.sh
