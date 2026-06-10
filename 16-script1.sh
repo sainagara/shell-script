6fc662e6d7548a50820ffc9bd17512a0d4b94d94
sainagaraju <nagarajusai91@gmail.com>
2026-06-11 02:51:16 +0530

    calling one script from another script using sh

---
+++ 16-script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR_PATH="/etc/paaswd"
+
+echo "Printing from script 1 directory path: $DIR_PATH"
+echo "Script-1 PID is: $$"
+
+sh 17-script2.sh
---
+++ 17-script2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "This is Script-2 directory path : $DIR_PATH"
+
+echo " Script-2 PID is: $$"
