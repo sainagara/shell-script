0ba86c992217b1ba8f0faca52099a6e3db550c1d
sainagaraju <nagarajusai91@gmail.com>
2026-06-11 19:20:21 +0530

    Sending a Mail on High Disk Utilzation Alert

--- 22-mail.sh
+++ 22-mail.sh
@@ -9,8 +9,12 @@ SUBJECT=$6
 
 FINAL_MESSAGE=$(echo "$MESSAGE" | sed -e "s/[&/]/\\&/g")
 
+echo "$FINAL_MESSAGE"
+
 FINAL_CONTEXT=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" \
     -e "s/MESSAGE/$FINAL_MESSAGE/g" -e "s/SUBJECT/$SUBJECT/g" template.html)
+
+
 
 {
 	echo "To: $TO_ADDRESS"
