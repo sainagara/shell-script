275e2dfa2e225ad4633970291abcb991e542d2d9
sainagaraju <nagarajusai91@gmail.com>
2026-06-11 18:49:14 +0530

    Sending a Mail on High Disk Utilzation Alert

--- 21-disk-utilization.sh
+++ 21-disk-utilization.sh
@@ -7,7 +7,7 @@ SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/)
 
 while IFS= read -r line
 do 
-     USAGE=$(echo line | awk '{print $6}' | cut -d "%d" f1)
+     USAGE=$(echo line | awk '{print $6}' | cut -d "%" f1)
      PARTION=$(echo line | awk '{print $7}')
  
 	if [ "$USAGE" -gt "$THRESHHOLD" ];then
@@ -17,12 +17,9 @@ do
 
 done <<< "$DISK_USAGE"
 
-sh 22-mail.sh "DevOps Team" "High Disk Usage" "$SERVER_IP" "$MESSAGE" "psainr55@gmail.com" "High Disk Usage Alert"
+if [ -n "$MESSAGE" ]; then
+   sh 22-mail.sh "DevOps Team" "High Disk Usage" "$SERVER_IP" "$MESSAGE" "psainr55@gmail.com" "High Disk Usage Alert"
+else
+   echo "No isuues with disl Udage"
 
-
-TO_TEAM=$1
-ALERT_TYPE=$2
-SERVER_IP=$3
-MESSAGE=$4
-TO_ADDRESS=$5
-SUBJECT=$6
+fi
